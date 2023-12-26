a2fc1764abb51cf4d3ce1c21906429ee81a48ebc
thawtar <mr.thaw.tar1990@gmail.com>
2023-12-26 11:51:05 +0900

    adjusted for v0.1.3

--- ButaChanRL/__init__.py
+++ ButaChanRL/__init__.py
@@ -4,11 +4,11 @@ ButaChanRL
 A minimalist reinforcement learning library based on PyTorch.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = 'Thaw Tar'
 __credits__ = 'Reinforcement Learning course by University of Alberta'
 
-from butachanrl.ExperienceReplay import ExperienceReplay
-from butachanrl.DQNAgent import DQNAgent
-from butachanrl.Network import DQN, DuelinDQN, ActorCritic
-from butachanrl.RL import RL
+#from butachanrl.ExperienceReplay import ExperienceReplay
+#from butachanrl.DQNAgent import DQNAgent
+#from butachanrl.Network import DQN, DuelinDQN, ActorCritic
+#from butachanrl.RL import RL
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='butachanrl',
-    version='0.1.2',
+    version='0.1.3',
     packages=["butachanrl","examples"],#find_packages(),
 
     author='ThawTar',
